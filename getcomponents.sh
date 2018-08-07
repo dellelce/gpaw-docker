c66c57925383c67530e24a38fa45cd3a7571dd83
Antonio Dell'Elce <antouk@dellelce.com>
2018-08-08 00:48:12 +0100

    lapack "-fPIC" version

--- getcomponents.sh
+++ getcomponents.sh
@@ -10,7 +10,7 @@ fileMap()
 #automake   https://github.com/TheApacheCats/automake/archive/1.15.tar.gz
  cat << EOF
 blas       https://bitbucket.org/antoniodellelce/blas/get/3.5.0-2017.tar.bz2
-lapack     https://bitbucket.org/antoniodellelce/lapack/get/lapack-3.5.0.tar.bz2
+lapack     https://bitbucket.org/antoniodellelce/lapack/get/lapack-3.5.0-fpic.tar.bz2
 libxc      https://bitbucket.org/antoniodellelce/libxc/get/libxc-3.0.0.tar.bz2
 EOF
 }
