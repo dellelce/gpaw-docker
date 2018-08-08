b8d8f05937dfb794de877b1c29bdea12dfa944f4
Antonio Dell'Elce <antouk@dellelce.com>
2018-08-08 12:22:00 +0100

    remove unneeded lines

--- install.sh
+++ install.sh
@@ -111,13 +111,11 @@ lapack_test()
  export LAPACK_SRC="$GPAW/source/lapack"
  export BLAS="$GPAW/software/lib"
 
- export CFLAGS="-fPIC" # trying to force everything to be "shared" :) am I too dirty?
  # unneeded
  #CFLAGS="-I${workDir}/include"  \
  #LDFLAGS="-L${workDir}/lib"     \
  pip install -U numpy   &&
  pip install -U ase     &&
- pip install -U gpaw	&&
  pip install -U setuptools_scm	&&
  pip install -U -r $GPAW/requirements.txt
 
