e755172403afa9f59f97a540a5c097ace8ae2948
Antonio Dell'Elce <antouk@dellelce.com>
2019-06-21 18:32:17 +0100

    add versions specific to this build, not only latest!

--- docker-push.sh
+++ docker-push.sh
@@ -2,24 +2,20 @@
 #
 # File:         docker-push.sh
 # Created:      100818
-# Description:  description for docker-push.sh
+# Description:
 #
 
 ### FUNCTIONS ###
 
  docker_hub()
  {
-  typeset target="$1"
-
   [ -z "$DOCKER_PASSWORD" -o -z "$DOCKER_USERNAME" ] && { echo "docker_hub: Docker environment not set-up correctly"; return 1; }
   echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
   rc=$?
   [ $rc -ne 0 ] && { echo "docker_hub: Docker hub login failed with rc = $rc"; return $rc; }
 
-  [ ! -z "$target" ] && { docker push "$target"; return $?; }
   return 0
  }
-
 
 ### ENV ###
 
@@ -27,6 +23,21 @@
 
 ### MAIN ###
 
- docker_hub "$image"
+ docker_hub || exit $?
+
+ # we expect an image without a version at this point
+ py_version=$(docker run -it --rm "$image" python -V | awk ' { print $2 } ')
+ gpaw_version=$(docker run -it --rm "$image" gpaw --version | awk -F- ' {print $2 } '(
+
+ # build image list
+ images="$image $image-${gpaw_version} $image-${gpaw_version}-py${py_version}"
+
+ docker tag "$image" "$image-${gpaw_version}"
+ docker tag "$image" "$image-${gpaw_version}-py${py_version}"
+
+ for image in $images
+ do
+   docker push "$image" || exit $?
+ done
 
 ### EOF ###
